50ed748ee746c35f71c0a13073e2101847afff4c
연가은 <108983937+kaeun0813@users.noreply.github.com>
2025-08-14 01:28:41 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.06 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,8 +1,13 @@
-
+#처음 내 풀이
+"""
 def solution(numbers):
     answer=[]
     i=0
     while i <len(numbers):
         answer.append(numbers[i]*2)
         i+=1
-    return answer
+    return answer"""
+
+#다른 사람 풀이 참고
+def solution(numbers):
+    return [num*2 for num in numbers]
