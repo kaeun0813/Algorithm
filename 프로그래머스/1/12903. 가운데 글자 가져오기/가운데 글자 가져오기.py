5ea66558686c018a6357c7275e463397c35b16f7
연가은 <108983937+kaeun0813@users.noreply.github.com>
2025-08-14 02:26:17 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,5 +1,11 @@
+#처음 코드
+"""
 def solution(s):
     if len(s)%2==0:
         return s[len(s)//2-1]+s[len(s)//2]
     else:
-        return s[len(s)//2]
+        return s[len(s)//2]"""
+    
+#개선 코드
+def solution(s):
+    return s[(len(s)-1)//2:len(s)//2+1]
