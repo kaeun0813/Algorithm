64b663bbb750ef33b55b7b2a760097287707247f
연가은 <108983937+kaeun0813@users.noreply.github.com>
2025-08-15 01:27:49 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.00 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,7 +1,12 @@
+#처음 코드
+"""
 def solution(n):
     answer = []
     i=1
     while i<=n:
         answer.append(i)
         i+=2
-    return answer
+    return answer"""
+
+def solution(n):
+    return list(range(1, n+1, 2))
