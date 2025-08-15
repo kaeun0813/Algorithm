a6f6360d8ddae4486662ea2c4e93e9ed6393ac0f
연가은 <108983937+kaeun0813@users.noreply.github.com>
2025-08-16 00:48:57 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,3 +1,5 @@
+#처음 코드
+"""
 def solution(slice, n):
 
     if slice < n:
@@ -7,3 +9,8 @@ def solution(slice, n):
             return n//slice+1
     else: 
         return 1
+"""
+#개선
+
+def solution(slice, n):
+    return ((n - 1) // slice) + 1
