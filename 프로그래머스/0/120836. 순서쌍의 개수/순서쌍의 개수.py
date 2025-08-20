edbcb6732fb09ad73f6f53ab1d6b2ec31b53f51f
연가은 <108983937+kaeun0813@users.noreply.github.com>
2025-08-20 10:36:36 -0400

    [level 0] Title: 순서쌍의 개수, Time: 0.06 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -0,0 +1,11 @@
+import math
+def solution(n):
+    answer = 0
+    for i in range(1, int(math.isqrt(n))+1):
+        if n%i==0:
+            if i*i ==n:
+                answer+=1
+            else:
+                answer+=2
+
+    return answer
