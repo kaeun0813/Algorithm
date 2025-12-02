06d9fa8e477edce3140cf7c5ef95fe093caa6561
연가은 <108983937+kaeun0813@users.noreply.github.com>
2025-12-02 14:30:53 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,15 @@
+N=[]
+P=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+for i in range(28):
+    N.append(int(input()))
+
+
+N.sort()
+#print(N)
+
+for i in range(28):
+    P.remove(N[i])
+print(P[0])
+print(P[1])
+    
+
