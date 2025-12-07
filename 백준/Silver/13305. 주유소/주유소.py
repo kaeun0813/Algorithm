8b7ae1387f1819654f1797d49d35aeefd8c18d2d
연가은 <108983937+kaeun0813@users.noreply.github.com>
2025-12-07 18:29:48 +0900

    [Silver III] Title: 주유소, Time: 104 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -3,16 +3,11 @@ length=list(map(int,input().split())) # 도시 사이 간의 도로 길이 list
 price=list(map(int,input().split())) # 각 도시마다 기름 값 list
 
 total=0 #최소 비용
+min_price=price[0]
 
 for i in range(n-1):
-    if i ==0: #일단 출발을 하려면 처음 도시에서 기름을 무조건 넣어야 한다. 
-        total+=price[i]*length[i]
-        continue
-    if price[i] <price[i-1]:# 이전 도시의 기름이 더 싸면 그 도시에서 기름 채우기
-        total+=price[i]*length[i]
-    else: # 이전 도시의 기름이 더 싸면 그 도시에서 기름 채우기
-        total+=price[i-1]*length[i]
+    if price[i] < min_price:# 이전 도시의 기름이 더 싸면 그 도시 기름값으로 바꾸기       
+        min_price=price[i]
+    total+=min_price*length[i]
+
 print(total)
-
-
-
