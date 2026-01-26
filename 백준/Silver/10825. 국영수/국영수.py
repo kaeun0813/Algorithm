29bf30bd2c79db9af68d7873bacbd57e8efb60c2
Kaeun Yeon <108983937+kaeun0813@users.noreply.github.com>
2026-01-26 15:18:10 +0900

    [Silver IV] Title: 국영수, Time: 2784 ms, Memory: 63432 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,8 @@
+n=int(input())
+array=[]
+for i in range(n):
+    data=input().split()
+    array.append((data[0],int(data[1]),int(data[2]),int(data[3])))
+array.sort( key=lambda x:(-x[1], x[2],-x[3],x[0]))
+for i in range(n):
+    print(array[i][0])
