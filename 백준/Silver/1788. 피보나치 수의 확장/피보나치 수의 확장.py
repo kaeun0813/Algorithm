94fbe5a9a711793393765d640da88180b5369a44
Kaeun Yeon <108983937+kaeun0813@users.noreply.github.com>
2026-02-11 13:34:43 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 348 ms, Memory: 71968 KB -BaekjoonHub

---
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
@@ -0,0 +1,21 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+target=abs(n)
+
+d=[0]*(target+1)
+if target>=1:
+    d[1]=1
+for i in range(2,target+1):
+    d[i]=(d[i-1]+d[i-2])%1000000000
+if n>0:
+    print(1)
+elif n<0:
+    if n%2==0:
+        print(-1)
+    else:
+        print(1)
+else:
+    print(0)
+
+print(d[target])
